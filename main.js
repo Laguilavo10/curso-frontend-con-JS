a97a21f7b5a392cd14319a852415546bd51125ed
Andres Laguilavo <lagui2003@gmail.com>
2022-08-23 22:32:56 -0500

    Integrando todos los toggles em conjunto, al igual, integrando el apartado de carrito de compras

--- main.js
+++ main.js
@@ -4,13 +4,42 @@ let infoUser = document.querySelector(".desktop-menu")
 let mobileMenu = document.querySelector(".mobile-menu")
 let menuBurguer = document.querySelector(".menu")
 
+let carritoIcon = document.querySelector(".navbar-shopping-cart")
+let infoCarrito = document.querySelector(".product-detail")
 
 
-user.addEventListener("click", () =>{desplegarMenus(infoUser)})
-menuBurguer.addEventListener("click", () =>{desplegarMenus(mobileMenu)})
+
+user.addEventListener("click", () =>{
+    desplegarMenus(infoUser)
+    if (!infoCarrito.classList.contains("inactive")) {
+        infoCarrito.classList.add("inactive")
+    }
+
+})
+
+menuBurguer.addEventListener("click", () =>{
+    desplegarMenus(mobileMenu)
+    if (!infoCarrito.classList.contains("inactive")) {
+        infoCarrito.classList.add("inactive")
+    }
+
+})
+
+carritoIcon.addEventListener("click", () =>{
+    desplegarMenus(infoCarrito) 
+    
+    if (!mobileMenu.classList.contains("inactive")) {
+        mobileMenu.classList.add("inactive")   
+    }
+    
+    if (!infoUser.classList.contains("inactive")) {
+        infoUser.classList.add("inactive")
+    }
 
 
-// 
+})
+
+
 // function inactiveMenu(params) {
 //inteto propio//
     // if (infoUser.classList.contains('inactive')) {
