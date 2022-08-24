b5a42f89124b9351d8b05e994def501d92242677
Andres Laguilavo <lagui2003@gmail.com>
2022-08-23 20:51:21 -0500

    Incluyendo la informacion correspondiente al menu-mobile

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
 let user = document.querySelector(".navbar-email")
 let infoUser = document.querySelector(".desktop-menu")
 
-console.log({infoUser});
+let mobileMenu = document.querySelector(".mobile-menu")
+let menuBurguer = document.querySelector(".menu")
 
 
-user.addEventListener("click", inactiveMenu)
 
-function inactiveMenu(params) {
-    //inteto propio
+user.addEventListener("click", () =>{desplegarMenus(infoUser)})
+menuBurguer.addEventListener("click", () =>{desplegarMenus(mobileMenu)})
+
+
+// 
+// function inactiveMenu(params) {
+//inteto propio//
     // if (infoUser.classList.contains('inactive')) {
     //     infoUser.classList.remove('inactive')
     // } else {
     //     infoUser.classList.add('inactive')
     // }
 
-    //repsuesta de juandc
-    infoUser.classList.toggle('inactive')
+//respuesta de juandc
+    // infoUser.classList.toggle('inactive')
+// }
+
+function desplegarMenus(cambio) {
+    cambio.classList.toggle('inactive')
 }
